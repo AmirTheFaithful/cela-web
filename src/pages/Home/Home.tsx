206ae61208f2ac2ff2105beb95d079f05bfaf806
Amir Daneshmand <amirdanesh02@gmail.com>
2025-07-26 01:34:38 +0330

    feat(pages/home): Add basic version of hero section to the home-page.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,11 @@
 import styles from "./home.module.scss";
 
+import { Hero } from "./elements/Hero";
+
 export const HomePage = () => {
-  return <main className={styles.home}>Hello, Cela!</main>;
+  return (
+    <main className={styles.home}>
+      <Hero />
+    </main>
+  );
 };
