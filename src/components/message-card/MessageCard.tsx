12e962ea59cc5fdc876318ff7de735761e4d2e45
Amir Daneshmand <amirdanesh02@gmail.com>
2025-07-29 00:31:20 +0330

    feat(components/message-card): Add support for sent, received and read statuses

--- src/components/message-card/MessageCard.tsx
+++ src/components/message-card/MessageCard.tsx
@@ -1,12 +1,36 @@
 import styles from "./message-card.module.scss";
 
-export const MessageCard = ({ content }: { content: string }) => {
+import CheckIcon from "../../assets/check.svg?react";
+import DoubleCheckIcon from "../../assets/double-check.svg?react";
+
+interface Props {
+  content: string;
+  status: "sent" | "received" | "read";
+  date: Date;
+  colors: string[];
+}
+
+export const MessageCard = ({ content, status, date, colors }: Props) => {
   return (
-    <div className={styles.messageCardContainer}>
+    <div
+      className={styles.messageCardContainer}
+      style={{
+        backgroundImage: `linear-gradient(135deg, ${colors.map(
+          (color) => `${color}`
+        )})`,
+      }}
+    >
       <div className={styles.messageCard}>
         <p className={styles.textContent}>{content}</p>
         <div className={styles.meta}>
-          <time className={styles.date}>09:37</time>
+          {status === "sent" && <CheckIcon className={styles.sent} />}
+          {status === "received" && (
+            <DoubleCheckIcon className={styles.received} />
+          )}
+          {status === "read" && <DoubleCheckIcon className={styles.read} />}
+          <time
+            className={styles.date}
+          >{`${date.getHours()}:${date.getMinutes()}`}</time>
         </div>
       </div>
     </div>
--- src/pages/Preview/index.tsx
+++ src/pages/Preview/index.tsx
@@ -2,12 +2,18 @@ import styles from "./preview.module.scss";
 
 import { MessageCard } from "../../components/message-card";
 
+// Hard-coded and dummy values are temporary.
 const dummyMessages = ["Hi! How's it going?"];
 
 export const PreviewPage = () => {
   return (
     <main className={styles.previewPage}>
-      <MessageCard content={dummyMessages[0]} />
+      <MessageCard
+        content={dummyMessages[0]}
+        status="read"
+        date={new Date()}
+        colors={["#ffcc99", "#a3d2ca", "#7e7dc9", "#faacea"]}
+      />
     </main>
   );
 };
